043c28d48a24bf6ea0e4fc7e2289d13f4c0ce36e
nyima6999 <nyima6999@gmail.com>
2022-05-15 16:15:05 -0700

    express server created and running

---
+++ Backend/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
